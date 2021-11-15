4d2d527ce81a9ccf98832e3b2a0a55105f70791e
Quinn Shultz <vanoxite@gmail.com>
2021-11-15 15:57:09 -0700

    Added required files (ready for instantiation)

---
+++ fa/nfa/NFA.java
@@ -0,0 +1,11 @@
+package fa.nfa;
+
+import fa.nfa.NFAInterface;
+
+/**
+ * Nodeterministic Finite Automata
+ * @author Quinn Shultz, Mateo Ortegon
+ */
+public abstract class NFA implements NFAInterface {
+
+}
---
+++ fa/nfa/NFAState.java
@@ -0,0 +1,11 @@
+package fa.nfa;
+
+import fa.State;
+
+/**
+ * A NFA State.
+ * @author Quinn Shultz, Mateo Ortegon
+ */
+public class NFAState extends State {
+
+}
